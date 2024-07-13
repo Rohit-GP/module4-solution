2fa714908c93f4484f06f26bc993b33e04e41cff
Rohit-GP <gp.rohit1201@gmail.com>
2024-07-13 10:37:42 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 (function (){
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Joseph", "Shiva", "Paul", "Franklin", "Pradeep", "Roshan", "Jim", "Lashmi", "Jasmine"];
   for (var name in names) {
     var firstLetter = names[name].charAt(0);    
     if (firstLetter.toLowerCase() == 'j') {
